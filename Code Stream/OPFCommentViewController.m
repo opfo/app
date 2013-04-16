4c7fc4b64f645a4a50a4e49933108f8978ba67a3
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-16 22:33:42 +0200

    Add files and boilerplate for custom comment view cells.

---
+++ Code Stream/OPFCommentViewController.m
@@ -0,0 +1,29 @@
+//
+//  OPFCommentViewController.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 16.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFCommentViewController.h"
+
+@implementation OPFCommentViewController
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
