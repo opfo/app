570b58e619140b67a951745849a286ce183403d3
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-16 14:40:13 +0200

    Add properties to User model

--- Code Stream/Models/User.h
+++ Code Stream/Models/User.h
@@ -11,4 +11,18 @@
 
 @interface User : MTLModel
 
+@property (nonatomic, assign, readonly) NSInteger identifier;
+@property (nonatomic, assign) NSInteger reputation;
+@property (nonatomic, copy, readonly) NSDate* creationDate;
+@property (nonatomic, copy) NSString* displayName;
+@property (nonatomic, copy) NSString* emailHash;
+@property (nonatomic, copy) NSDate* lastAccessDate;
+@property (nonatomic, copy) NSString* websiteUrl;
+@property (nonatomic, copy) NSString* location;
+@property (nonatomic, assign) NSInteger age;
+@property (nonatomic, copy) NSString* aboutMe;
+@property (nonatomic, assign) NSInteger view;
+@property (nonatomic, assign) NSInteger upVotes;
+@property (nonatomic, assign) NSInteger downVotes;
+
 @end
