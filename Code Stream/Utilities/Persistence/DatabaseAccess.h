df6bea6025750a0af586d602a407dac6a0bb978c
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-16 15:20:24 +0200

    Add database and database access stubs

---
+++ Code Stream/Utilities/Persistence/DatabaseAccess.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseAccess.h
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-04-16.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseAccess : NSObject
+
+@end
---
+++ Code Stream/Utilities/Persistence/DatabaseAccess.m
@@ -0,0 +1,13 @@
+//
+//  DatabaseAccess.m
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-04-16.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "DatabaseAccess.h"
+
+@implementation DatabaseAccess
+
+@end
