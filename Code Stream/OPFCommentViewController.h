6af4751f889f1b805368daaff0a2bec4ef2713d5
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-16 21:46:41 +0200

    Add boilerplate files for comment view.

---
+++ Code Stream/OPFCommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  OPFCommentViewController.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 16.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFCommentViewController : UITableViewController
+
+@end
