39df809abece4570911e1ce4833d6dbf35814ab2
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-17 20:26:53 +0200

    Add views and controller for comment's header.

---
+++ Code Stream/OPFCommentViewHeaderController.h
@@ -0,0 +1,13 @@
+//
+//  OPFCommentViewHeaderController.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 17.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFCommentViewHeaderController : UIViewController
+
+@end
---
+++ Code Stream/OPFCommentViewHeaderController.m
@@ -0,0 +1,38 @@
+//
+//  OPFCommentViewHeaderController.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 17.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFCommentViewHeaderController.h"
+
+@interface OPFCommentViewHeaderController ()
+
+@end
+
+@implementation OPFCommentViewHeaderController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
