e41883b58e5ee560bbf47b21785453fc122e3506
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-17 22:07:27 +0200

    Add header view, xib and links them together.

---
+++ Code Stream/OPFCommentViewHeader.h
@@ -0,0 +1,24 @@
+//
+//  OPFCommentViewHeader.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 17.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFCommentViewHeader : UIView
+
+@property(nonatomic, weak) IBOutlet UILabel *postTitle;
+@property(nonatomic, weak) IBOutlet UILabel *postUserName;
+@property(nonatomic, weak) IBOutlet UILabel *postDate;
+@property(nonatomic, weak) IBOutlet UILabel *postTime;
+@property(nonatomic, weak) IBOutlet UIImageView *userAvatar;
+@property(nonatomic, weak) IBOutlet UILabel *postVoteCount;
+
+@property(nonatomic, strong) NSObject *postModel;
+
+- (void)setModelValuesInView;
+
+@end
---
+++ Code Stream/OPFCommentViewHeader.m
@@ -0,0 +1,31 @@
+//
+//  OPFCommentViewHeader.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 17.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFCommentViewHeader.h"
+
+@implementation OPFCommentViewHeader
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setModelValuesInView
+{
+//    self.postTitle.text = self.postModel.title;
+//    self.postUserName.text = self.postModel.userName;
+//    self.postDate.text = self.postModel.date;
+//    self.postTime.text = self.postModel.time;
+//    self.postVoteCount.text = self.postModel.voteCount;
+}
+
+@end
