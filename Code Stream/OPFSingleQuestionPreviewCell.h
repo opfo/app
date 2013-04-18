1bc9f42de0e39b2457a8246700e94f4558d0c37d
Martin Goth <gothm@tf.uni-freiburg.de>
2013-04-18 10:46:16 +0200

    xib-File for SingleQuestionPreview

---
+++ Code Stream/OPFSingleQuestionPreviewCell.h
@@ -0,0 +1,13 @@
+//
+//  OPFSingleQuestionPreviewCell.h
+//  Code Stream
+//
+//  Created by Martin Goth on 2013-04-18.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFSingleQuestionPreviewCell : UITableViewCell
+
+@end
---
+++ Code Stream/OPFSingleQuestionPreviewCell.m
@@ -0,0 +1,29 @@
+//
+//  OPFSingleQuestionPreviewCell.m
+//  Code Stream
+//
+//  Created by Martin Goth on 2013-04-18.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFSingleQuestionPreviewCell.h"
+
+@implementation OPFSingleQuestionPreviewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
