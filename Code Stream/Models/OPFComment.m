a35238f7a9de03456c6309e6b5008dcb64106058
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-18 16:06:25 +0200

    Add comment model with spec

---
+++ Code Stream/Models/OPFComment.m
@@ -0,0 +1,28 @@
+//
+//  OPFComment.m
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-04-18.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFComment.h"
+
+@implementation OPFComment
+
++ (NSArray *) all
+{
+    return nil;
+}
+
++ (NSArray *) where:(NSDictionary *)attributes
+{
+    return nil;
+}
+
++ (id) find:(NSInteger)identifier
+{
+    return nil;
+}
+
+@end
---
+++ Code StreamTests/ModelSpecs/OPFCommentSpec.m
@@ -0,0 +1,35 @@
+//
+//  OPFCommentSpec.m
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-04-18.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "OPFComment.h"
+
+SpecBegin(OPFComment)
+
+describe(@"Model creation", ^{
+    __block OPFComment* comment;
+    __block NSDictionary* properties = @{@"score": @(9), @"text": @"lorem ipsum", @"creationDate": [NSDate date]};
+    
+    it(@"should be possible using a dictionary", ^{
+        NSError* error;
+        comment = [[OPFComment alloc] initWithDictionary: properties error: &error];
+        expect(comment).toNot.equal(nil);
+        expect(comment.score).to.equal(@(9));
+        expect(comment.text).to.equal(properties[@"text"]);
+        expect(comment.creationDate).to.equal(properties[@"creationDate"]);
+    });
+    
+    it(@"should be possible using the database", ^{
+        OPFComment* comment = [OPFComment find: 10393284];
+        expect(comment).toNot.equal(nil);
+    });
+});
+
+SpecEnd
