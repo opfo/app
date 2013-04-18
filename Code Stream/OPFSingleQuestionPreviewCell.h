658f67fbd55801311ce244f65754134d89fb27e0
Martin Goth <gothm@tf.uni-freiburg.de>
2013-04-18 15:21:56 +0200

    Integrate preview cells to view controller
    Add the view controller to the storyboard
    Accepted Answer is shown
    Placeholder Examples are displayed in table view

--- Code Stream/OPFSingleQuestionPreviewCell.h
+++ Code Stream/OPFSingleQuestionPreviewCell.h
@@ -10,4 +10,17 @@
 
 @interface OPFSingleQuestionPreviewCell : UITableViewCell
 
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, assign) NSInteger views;
+@property (nonatomic, assign) NSInteger answers;
+@property (nonatomic, copy) NSArray *tags;
+@property (nonatomic, assign) BOOL acceptedAnswer;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *viewsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answersLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *acceptedAnswerImage;
+
+
 @end
--- Code Stream/OPFSingleQuestionPreviewCell.m
+++ Code Stream/OPFSingleQuestionPreviewCell.m
@@ -9,6 +9,37 @@
 #import "OPFSingleQuestionPreviewCell.h"
 
 @implementation OPFSingleQuestionPreviewCell
+@synthesize views = _views;
+@synthesize title = _title;
+@synthesize answers = _answers;
+
+- (void)setAcceptedAnswer:(BOOL)acceptedAnswer {
+	self.acceptedAnswerImage.hidden = !acceptedAnswer;
+}
+
+- (BOOL)acceptedAnswer {
+	return !self.acceptedAnswerImage.hidden;
+}
+
+- (void)setViews:(NSInteger)Views {
+	_views = Views;
+	
+	if (Views >= 1000) {
+		self.viewsLabel.text = [NSString stringWithFormat: @"%1.1fk", (double)Views / 1000.0];
+	} else {
+		self.viewsLabel.text = [NSString stringWithFormat:@"%d", Views];
+	}
+}
+
+- (void)setAnswers:(NSInteger)Answers {
+	_answers = Answers;
+	self.answersLabel.text = [NSNumber numberWithInteger:Answers].stringValue;
+}
+
+- (void)setTitle:(NSString *)Title {
+	_title = Title;
+	self.questionLabel.text = Title;
+}
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -16,14 +47,18 @@
     if (self) {
         // Initialization code
     }
+	
+	
     return self;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-
+	
     // Configure the view for the selected state
 }
+
+
 
 @end
