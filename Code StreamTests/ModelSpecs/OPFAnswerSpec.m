3538c00fc6b3c191bdd8b6bff9758ee4581835d2
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-23 13:43:42 +0200

    Remove the ugly duplicate spec

--- Code StreamTests/ModelSpecs/OPFAnswerSpec.m
+++ Code StreamTests/ModelSpecs/OPFAnswerSpec.m
@@ -6,8 +6,36 @@
 //  Copyright (c) 2013 Opposing Force. All rights reserved.
 //
 
-#import "OPFAnswerSpec.h"
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "OPFModelSpecHelper.h"
+#import "OPFQuestion.h"
+#import "OPFAnswer.h"
 
-@implementation OPFAnswerSpec
+SpecBegin(OPFAnswer)
 
-@end
+describe(@"Fetching", ^{
+    __block OPFAnswer* answer;
+    
+    beforeEach(^{
+        answer = [[[OPFAnswer query] whereColumn:@"id" is:@(8474366)] getOne];
+    });
+    it(@"should not fetch an incorrect type of model", ^{
+        answer = [[[OPFAnswer query] whereColumn: @"id" is: @(8414076)] getOne];
+        expect(answer).to.beNil();
+    });
+    
+    it(@"should fetch an object of the correct type", ^{
+        expect(answer).to.beKindOf([OPFAnswer class]);
+    });
+    
+    it(@"should be able to fetch the connected question", ^{
+        expect(answer.parent).notTo.beNil();
+        expect(answer.parent).to.beKindOf([OPFQuestion class]);
+        expect(answer.parent.identifier).to.equal(answer.parentId);
+    });
+    
+});
+
+SpecEnd
