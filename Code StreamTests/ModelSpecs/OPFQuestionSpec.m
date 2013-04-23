4a104369f3e6e298b4c72ccffee172c1702767cd
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-23 10:57:46 +0200

    Add a spec for questions

---
+++ Code StreamTests/ModelSpecs/OPFQuestionSpec.m
@@ -0,0 +1,24 @@
+//
+//  OPFQuestionSpec.m
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-04-23.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "OPFModelSpecHelper.h"
+#import "OPFQuestion.h"
+
+SpecBegin(OPFQuestion)
+
+describe(@"Fetching", ^{
+    it(@"should not fetch an incorrect type of model", ^{
+        id question = [[[OPFQuestion query] whereColumn: @"id" is: @"8474693"] getOne];
+        expect(question).to.beNil();
+    });
+});
+
+SpecEnd
