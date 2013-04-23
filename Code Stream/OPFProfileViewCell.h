db698bf974ab255cabe85fcdac531a31009d0445
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-23 10:45:04 +0200

    Add profile view cell *.xib and *.m/*.h-files.

---
+++ Code Stream/OPFProfileViewCell.h
@@ -0,0 +1,13 @@
+//
+//  OPFProfileViewCell.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 23.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFProfileViewCell : UITableViewCell
+
+@end
---
+++ Code Stream/OPFProfileViewCell.m
@@ -0,0 +1,29 @@
+//
+//  OPFProfileViewCell.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 23.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFProfileViewCell.h"
+
+@implementation OPFProfileViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
