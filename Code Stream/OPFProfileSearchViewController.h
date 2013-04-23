770e6a7cdd89150544d77a880ad72cc996b854f3
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-23 10:55:26 +0200

    Add ui controller for profile search.

---
+++ Code Stream/OPFProfileSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  OPFProfileSearchViewController.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 23.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFProfileSearchViewController : UIViewController
+
+@end
---
+++ Code Stream/OPFProfileSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  OPFProfileSearchViewController.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 23.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFProfileSearchViewController.h"
+
+@interface OPFProfileSearchViewController ()
+
+@end
+
+@implementation OPFProfileSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
