a208e9f7b9d911714e918a449a4b822fb0403af6
Marcus Johansson <marcus1.johansson@gmail.com>
2013-04-29 14:15:27 +0200

    Solved the avatar resize by customizing the tablecell from scratch

--- Code Stream/OPFUserProfileViewController.h
+++ Code Stream/OPFUserProfileViewController.h
@@ -14,17 +14,22 @@
 
 + (instancetype)newFromStoryboard;
 
-
 @property (strong, nonatomic) OPFUser *user;
-@property (weak, nonatomic) IBOutlet UILabel *userDisplayName;
-@property (weak, nonatomic) IBOutlet UITextView *userAboutMe;
+//@property (weak, nonatomic) IBOutlet UILabel *userDisplayName;
+@property (weak, nonatomic) IBOutlet UILabel *userVotes;
+@property (weak, nonatomic) IBOutlet UILabel *views;
+@property (weak, nonatomic) IBOutlet UIWebView *userBio;
+@property (weak, nonatomic) IBOutlet UIImageView *userAvatar;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+
+
 @property (weak, nonatomic) IBOutlet UILabel *userLocation;
 @property (weak, nonatomic) IBOutlet UILabel *userReputation;
 @property (weak, nonatomic) IBOutlet UILabel *userCreationDate;
 @property (weak, nonatomic) IBOutlet UILabel *userLastAccess;
 @property (weak, nonatomic) IBOutlet UILabel *userAge;
 @property (weak, nonatomic) IBOutlet UILabel *userWebsite;
-@property (weak, nonatomic) IBOutlet UIImageView *userImage;
+//@property (weak, nonatomic) IBOutlet UIImageView *userImage;
 
 
 @end
