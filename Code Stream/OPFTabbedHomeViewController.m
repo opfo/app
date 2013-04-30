749657f4564beabdf9646f5ad548ec09f6db8cea
Tobias Deekens <tobias.deekens@gmail.com>
2013-04-30 10:58:53 +0200

    Add tabbed view controller as AKTabBarController.

---
+++ Code Stream/OPFTabbedHomeViewController.m
@@ -0,0 +1,59 @@
+//
+//  OPFTabbedHomeViewController.m
+//  Code Stream
+//
+//  Created by Tobias Deekens on 30.04.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFTabbedHomeViewController.h"
+#import "OPFQuestionsViewController.h"
+#import "OPFProfileSearchViewController.h"
+#import "OPFUserProfileViewController.h"
+
+@interface OPFTabbedHomeViewController ()
+
+@property(strong, nonatomic) OPFQuestionsViewController *questionsViewController;
+@property(strong, nonatomic) OPFProfileSearchViewController *profileSearchViewController;
+@property(strong, nonatomic) OPFUserProfileViewController *userProfileViewController;
+
+- (void)opfSetupView;
+
+@end
+
+@implementation OPFTabbedHomeViewController
+
+const int TabbedBarHeight = 60;
+
+- (id)init
+{
+    self = [super initWithTabBarHeight:TabbedBarHeight];
+    
+    if (self) {
+        [self opfSetupView];
+    }
+    return self;
+}
+
+- (void)opfSetupView
+{
+    self.questionsViewController = [OPFQuestionsViewController new];
+    self.profileSearchViewController = [OPFProfileSearchViewController new];
+    self.userProfileViewController = [OPFUserProfileViewController new];
+    
+    [self setViewControllers:[NSMutableArray arrayWithObjects:self.questionsViewController, self.profileSearchViewController, self.userProfileViewController, nil]];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
