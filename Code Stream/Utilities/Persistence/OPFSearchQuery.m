e77677a63741d64cdd415a71dfdd29df320a8fce
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-05-01 18:13:58 +0200

    Add spec and stub class for search query

---
+++ Code Stream/Utilities/Persistence/OPFSearchQuery.m
@@ -0,0 +1,17 @@
+//
+//  OPFSearchQuery.m
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-05-01.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFSearchQuery.h"
+
+@implementation OPFSearchQuery
+
++(OPFSearchQuery*) searchQuery {
+    return nil;
+}
+
+@end
---
+++ Code StreamTests/ModelSpecs/OPFPostSpec.h
@@ -0,0 +1,13 @@
+//
+//  OPFPostSpec.h
+//  Code Stream
+//
+//  Created by Jesper Josefsson on 2013-05-01.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OPFPostSpec : SenTestCase
+
+@end
