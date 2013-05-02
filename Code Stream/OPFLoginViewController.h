da969e374345b6aa690090a5c4b206f846eb5714
Tobias Deekens <tobias.deekens@gmail.com>
2013-05-02 12:56:55 +0200

    Add auto generated files to login and signup view.

---
+++ Code Stream/OPFLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  OPFLoginViewController.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 02.05.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFLoginViewController : UIViewController
+
+@end
