955036b63fe344573b6fdeb96fd2d3daf9af9399
Tobias Deekens <tobias.deekens@gmail.com>
2013-05-02 14:51:06 +0200

    Add title of favorite view controller.

--- Code Stream/OPFFavoritesViewController.m
+++ Code Stream/OPFFavoritesViewController.m
@@ -18,7 +18,7 @@
 {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
+        [self opfSetupView];
     }
     return self;
 }
@@ -26,18 +26,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)opfSetupView
+{
+    self.title = NSLocalizedString(@"My favorites", @"Favorite View controller title");
 }
 
 #pragma mark - TabbedViewController methods
