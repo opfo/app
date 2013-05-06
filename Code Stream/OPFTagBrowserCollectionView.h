c3cfcc142dc7ae30ac2acd5c689edea290e04d81
Tobias Deekens <tobias.deekens@gmail.com>
2013-05-06 16:10:19 +0200

    Replace favorites tab, add and move tag-browser tab instead.

---
+++ Code Stream/OPFTagBrowserCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  OPFTagBrowserCollectionView.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 06.05.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFTagBrowserCollectionView : UICollectionView;
+
+@end
