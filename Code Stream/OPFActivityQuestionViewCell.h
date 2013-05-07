5c5881571011582c76a32203adae401775889346
Tobias Deekens <tobias.deekens@gmail.com>
2013-05-07 11:03:58 +0200

    Change question cell's look in activitiy-view.

--- Code Stream/OPFActivityQuestionViewCell.h
+++ Code Stream/OPFActivityQuestionViewCell.h
@@ -14,6 +14,12 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *questionTitle;
 @property (weak, nonatomic) IBOutlet UITextView *questionBody;
+@property (weak, nonatomic) IBOutlet UIImageView *acceptedAnswer;
+@property (weak, nonatomic) IBOutlet UILabel *viewCount;
+@property (weak, nonatomic) IBOutlet UILabel *answerCount;
+@property (weak, nonatomic) IBOutlet UILabel *scoreCount;
+@property (weak, nonatomic) IBOutlet UILabel *commentCount;
+
 
 @property(nonatomic, strong) OPFQuestion *questionModel;
 
