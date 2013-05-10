6c3a4bb2005813638d49070f47889654401235bb
Marcus Johansson <marcus1.johansson@gmail.com>
2013-05-10 10:18:49 +0200

    Added class that will insert data in database

---
+++ Code Stream/OPFUpdateQuery.h
@@ -0,0 +1,13 @@
+//
+//  OPFUpdateQuery.h
+//  Code Stream
+//
+//  Created by Marcus Johansson on 2013-05-10.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPFUpdateQuery : NSObject
+
+@end
---
+++ Code Stream/OPFUpdateQuery.m
@@ -0,0 +1,18 @@
+//
+//  OPFUpdateQuery.m
+//  Code Stream
+//
+//  Created by Marcus Johansson on 2013-05-10.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import "OPFUpdateQuery.h"
+
+@implementation OPFUpdateQuery
+
+-(BOOL) updateWithQuestionTitle: (NSString *) title Body: (NSString *) body Tags: (NSArray *) tags{
+    
+    return NO;
+}
+
+@end
