139ced92ae4428fae4023470577d4e990d21925b
Tobias Deekens <tobias.deekens@gmail.com>
2013-05-10 11:50:57 +0200

    Add footer for tag-browser showing question-match-count.

---
+++ Code Stream/OPFTagBrowserFooter.h
@@ -0,0 +1,16 @@
+//
+//  OPFTagBrowserFooter.h
+//  Code Stream
+//
+//  Created by Tobias Deekens on 10.05.13.
+//  Copyright (c) 2013 Opposing Force. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFTagBrowserFooter : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *questionCount;
+@property (weak, nonatomic) IBOutlet UILabel *questionsLabel;
+
+@end
