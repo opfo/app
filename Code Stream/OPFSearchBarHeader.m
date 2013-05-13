0143e33c472d19ea16360cdd85331b8c4c0bd004
Martin Goth <gothm@tf.uni-freiburg.de>
2013-05-13 18:46:59 +0200

    Try to fix graphics

--- Code Stream/OPFSearchBarHeader.m
+++ Code Stream/OPFSearchBarHeader.m
@@ -7,7 +7,9 @@
 //
 
 #import "OPFSearchBarHeader.h"
-#import <BlocksKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @implementation OPFSearchBarHeader
 
@@ -44,6 +46,8 @@
 
 - (void)sharedInit {
 	self.displayedHeader = SearchBar;
+	
+    self.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"SearchBarGradient"]];
 }
 
 - (void)handleSwitchEvent:(UIButton *)sender {
