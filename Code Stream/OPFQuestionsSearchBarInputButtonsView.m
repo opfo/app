76b43cefc10f9cfebed1fe5b224cf85851f13855
Aron Cedercrantz <aron@cedercrantz.se>
2013-05-15 02:27:00 +0200

    Change font of “insert new token” buttons to app font.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

--- Code Stream/OPFQuestionsSearchBarInputButtonsView.m
+++ Code Stream/OPFQuestionsSearchBarInputButtonsView.m
@@ -7,6 +7,15 @@
 //
 
 #import "OPFQuestionsSearchBarInputButtonsView.h"
+#import "UIFont+OPFAppFonts.h"
 
 @implementation OPFQuestionsSearchBarInputButtonsView
+
+- (void)awakeFromNib
+{
+	UIFont *font = [UIFont opf_boldAppFontOfSize:15.f];
+	self.insertNewTagButton.titleLabel.font = font;
+	self.insertNewUserButton.titleLabel.font = font;
+}
+
 @end
